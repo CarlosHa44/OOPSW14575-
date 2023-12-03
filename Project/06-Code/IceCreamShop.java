77dcc80bd145269ce45bb5351b425e8189a63bd1
MateoJa54 <143151369+MateoJa54@users.noreply.github.com>
2023-12-03 18:42:12 -0500

    main version 1.1.3

--- Project/06-Code/IceCreamShop.java
+++ Project/06-Code/IceCreamShop.java
@@ -27,6 +27,40 @@ public class IceCreamShop {
             newProduct--;
         }
         System.out.println(products);
+        
+        // ingresar datos del cliente 
+        Scanner scanner = new Scanner(System.in);
+
+        // Crear un objeto GestionClient
+        GestionClient gestionClient = new GestionClient();
+
+        // Solicitar al usuario que ingrese los datos del cliente
+        System.out.println("Ingrese los datos del Cliente:");
+
+        System.out.print("ID: ");
+        int id = scanner.nextInt();
+        scanner.nextLine(); // Consumir la nueva línea
+
+        System.out.print("Nombre: ");
+        String name = scanner.nextLine();
+
+        System.out.print("Email: ");
+        String email = scanner.nextLine();
+
+        System.out.print("Número de teléfono: ");
+        String cellphoneNumber = scanner.nextLine();
+
+        System.out.print("¿Es del sur? (true/false): ");
+        boolean ifsouth = scanner.nextBoolean();
+
+        System.out.print("¿Es mayor de edad? (true/false): ");
+        boolean ifmajority = scanner.nextBoolean();
+
+        // Crear el objeto Cliente con los datos proporcionados
+        Client client = new Client(id, name, email, cellphoneNumber, ifsouth, ifmajority);
+
+        // Agregar el cliente a la lista de clientes en GestionClient
+        gestionClient.agregarCliente(client);
     }
 
     public static Product addProduct() {
